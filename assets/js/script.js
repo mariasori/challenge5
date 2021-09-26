b0c96584021e76190b5914958bf03f137ad8ce21
Maria Sori <mariasori.school@gmail.com>
2021-09-26 17:05:08 -0400

    saving input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-    //display current day 
-    $("#currentDay").text(moment().format("dddd MMMM Do YYYY")); // use of moment.js
+    //display current day
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    //save button event listener
+    $(".saveBtn").on("click", function () { 
+        // create variable for textarea
+        var text = $(this).siblings(".description").val();
+        //create variable for time-slot
+        var time = $(this).parent().attr("id"); 
 
+        //save variables in local storage
+        localStorage.setItem(time, text);
+    });
 });
-
